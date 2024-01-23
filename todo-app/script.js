70570f5a054c1255a106b6bd63ddc90090e5341e
long.danghoang <long.danghoang@proton.me>
2024-01-23 18:10:21 +0700

    Update JS with simple function

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,1 +1,66 @@
-console.log("Hello world");
+const todoInputElement = document.getElementById("todo-input");
+const todoListElement = document.getElementById("todo-list");
+let todoList = [];
+
+// Initialize app
+appInitialize();
+
+function appInitialize() {
+  getTodoListFromLocalStorage();
+  console.log(todoList);
+  for (todoItem of todoList) {
+    addTodoItemElement(todoItem);
+  }
+}
+
+todoInputElement.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    todoInputElement.value ? addTodoItem(todoInputElement.value) : null;
+    todoInputElement.value = "";
+  }
+});
+
+function addTodoItem(value) {
+  // Set up todoItem
+  let todoItem = {
+    id: Date.now(),
+    label: value,
+    status: "not-finished",
+  };
+  todoList.push(todoItem);
+
+  // Set up todoItemELement
+  addTodoItemElement(todoItem);
+
+  // Save data
+  saveTodoListToLocalStorage();
+
+  console.log(todoList);
+}
+
+function addTodoItemElement(todoItem) {
+  let todoItemElement = document.createElement("div");
+  todoItemElement.className = "todo-item";
+  todoItemElement.innerText = todoItem.label;
+  todoItemElement.id = `todo-item-${todoItem.id}`;
+  todoItemElement.addEventListener("click", onTodoItemClick);
+  todoListElement.insertBefore(todoItemElement, todoListElement.firstChild);
+}
+
+function onTodoItemClick(e) {
+  const itemId = parseInt(e.target.id.replace("todo-item-", ""));
+  todoList.map((item) => {
+    if (item.id === itemId) {
+      item.label += "Clicked";
+      e.target.innerText = item.label;
+    }
+  });
+}
+
+function saveTodoListToLocalStorage() {
+  localStorage.setItem("todo-list", JSON.stringify(todoList));
+}
+
+function getTodoListFromLocalStorage() {
+  todoList = JSON.parse(localStorage.getItem("todo-list")) ?? [];
+}
